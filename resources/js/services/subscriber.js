c0bfb3a75031f0cf1b5d2670b8f08ea0850ab3a6
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-05-03 14:49:45 -0400

    se elimina restricción de bdup para crear pacientes.

--- resources/js/services/subscriber.js
+++ resources/js/services/subscriber.js
@@ -1,10 +1,14 @@
 import store from '../store';
+import fetch from '../services/fetch';
 
 store.subscribe(mutation => {
     switch (mutation.type) {
         case 'auth/SET_ACCESS_TOKEN':
             if (mutation.payload) {
                 localStorage.setItem('access_token', mutation.payload);
+                fetch.Authorization = `Bearer ${localStorage.getItem(
+                    'access_token'
+                )}`;
             } else {
                 localStorage.removeItem('access_token');
             }
